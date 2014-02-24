fe4966b062138e2db57ec6f97036b6dffd18ffbb
Alexandre Bedrytski <alexandre@zenprog.fr>
2014-02-24 21:06:44 +0100

    Fixed error in Application controller and sbt version in the config

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,8 +8,9 @@ import views.html.*;
 
 import models.*;
 
+
 public class Application extends Controller {
-  static Form<Task> taskForm = form(Task.class); 
+  static Form<Task> taskForm = Form.form(Task.class); 
   
   public static Result index() {
     return redirect(routes.Application.tasks());
